338bff89483054c5289c49e6be024d0c7f91182d
Luis Eduardo <luis152288@gmail.com>
2020-09-05 13:35:51 -0400

    actualizando temperatura

---
+++ booking-export.php
@@ -0,0 +1,107 @@
+<?php
+require('../../../wp-load.php');
+require('fpdf/fpdf.php');
+
+$d = $_GET['date']; 
+$t = $_GET['time'];
+
+global $wpdb;
+$hora = date('h:i A', strtotime($t));
+$fecha = $d;
+
+$date = date("Y-m-d", strtotime($d));
+$time = date('H', strtotime($t));
+$time = $time + 5;
+
+$appointment = $wpdb->prefix . 'amelia_appointments';
+$booking = $wpdb->prefix . 'amelia_customer_bookings';
+$user = $wpdb->prefix . 'amelia_users';
+
+$query = "SELECT $booking.`id`, `bookingStart`,`bookingEnd`, `customFields`, `info`, `email` FROM $appointment, $booking, $user WHERE $appointment.`ID` = $booking.`appointmentId` AND $booking.`customerId` = $user.`id` AND `bookingStart` LIKE '%$date $time%' ORDER BY $booking.`customerId` ASC  ";
+
+$resQuery =  $wpdb->get_results($query);
+
+$fullTime = "{$fecha} {$hora}";
+
+class PDF extends FPDF{
+	// Cabecera de página
+	function Header(){
+		global $title;
+		$this->SetFont('Times', '', 14);
+		$this->Cell(80);	   
+    	$this->Cell(30,10, $title[0],0,0,'C');
+    	$this->Ln(5);
+	    $this->SetFont('Times', '', 10);
+	    $this->Cell(80);
+	    $this->Cell(30,10, $title[1],0,0,'C');
+	    $this->SetFont('Times','', 8);
+	    $this->Ln(15);
+	    $this->Cell(35, 5, 'Fecha Eucaristia', 1);
+	    $this->Cell(30, 5, $title[2], 1);
+	    $this->Ln();
+		$this->Cell(35, 5, 'Correo', 1);
+		$this->Cell(30, 5, 'Nombre', 1);
+		$this->Cell(23, 5, 'Telefono', 1);
+		$this->Cell(55, 5, 'Direccion', 1);
+		$this->Cell(30, 5, 'CC/CE/PASAPORTE', 1);
+		$this->Cell(10, 5, 'Edad', 1);
+		$this->Cell(12, 5, 'Temp...', 1);
+		$this->Ln();
+	}
+
+	// Pie de página
+	function Footer(){
+	    // Posición: a 1,5 cm del final
+	    $this->SetY(-15);
+	    // Arial italic 8
+	    //$this->SetFont('Arial','I',8);
+	    // Número de página
+	    $this->Cell(0,10,'Page '.$this->PageNo().'/{nb}',0,0,'C');
+	}
+}
+
+$title = get_bloginfo('name');
+$description = get_bloginfo('description');
+
+$title = [$title, $description, $fullTime];
+
+$pdf = new PDF();
+$pdf->AliasNbPages();
+$pdf->SetTitle($title);
+$pdf->AddPage();
+$pdf->SetFont('Arial', '', 6);
+foreach($resQuery as $d){
+	$customFields = json_decode($d->customFields);
+	$info = json_decode($d->info);
+	$pdf->Cell(35, 5, $d->email, 1);
+	$pdf->Cell(30, 5, substr("{$info->firstName} {$info->lastName}", 0, 20), 1);
+	$pdf->Cell(23, 5, $info->phone, 1);
+
+	if($customFields){
+		foreach ($customFields as $c) {
+			if ($c->label == "Direccion") {
+				$pdf->Cell(55, 5, substr($c->value, 0, 30), 1);
+			}
+
+			if ($c->label == "CC/CE/PASAPORTE") {
+				$pdf->Cell(30, 5, $c->value, 1);
+			}
+
+			if ($c->label == "Edad") {
+				$pdf->Cell(10, 5, $c->value, 1);
+			}
+
+			if ($c->label == "Temperatura") {
+				$pdf->Cell(12, 5, $c->value, 1);
+			}
+		}
+	}else{
+		$pdf->Cell(55, 5, " ", 1);
+		$pdf->Cell(35, 5, " ", 1);
+		$pdf->Cell(10, 5, " ", 1);
+		$pdf->Cell(10, 5, " ", 1);
+	}
+	
+	$pdf->Ln();
+}
+$pdf->Output();
--- export-reservation.php
+++ export-reservation.php
@@ -19,8 +19,6 @@
 */
 ini_set("session.auto_start", 0);
 
-require('fpdf/fpdf.php');
-
 function boot_session() {
   session_start();
 }
@@ -33,192 +31,23 @@ function exportReservationMenu() {
 
 	add_menu_page( 	__( 'Exportar reservacion', 'export-wp' ),
          			__( 'Exportar reservacion', 'export-wp' ), 
-			        'administrator', 
+			        'wpamelia-manager',
 			        'export',
-			        'get_date'
+			        'get_data'
 				);
 }
 
-function exportBooking($d, $t){
+function get_data() {
 	global $wpdb;
-	$hora = date('h:i A', strtotime($t));
-	$fecha = $d;
-
-	$date = date("Y-m-d", strtotime($d));
-	$time = date('H', strtotime($t));
-	$time = $time + 5;
-	
-	$appointment = $wpdb->prefix . 'amelia_appointments';
-	$booking = $wpdb->prefix . 'amelia_customer_bookings';
-
-	$query = " SELECT `bookingStart`,`bookingEnd`, `customFields`, `info` FROM $appointment INNER JOIN $booking ON $appointment.`ID` = $booking.`appointmentId` WHERE `bookingStart` LIKE '%$date $time%' ";
-
-	$resQuery =  $wpdb->get_results($query);
-
-	$fullTime = "{$fecha} {$hora}";
-
-	class PDF extends FPDF{
-		// Cabecera de página
-		function Header(){
-			$this->SetFont('Times','', 8);
-			$this->Cell(30, 5, 'Reserva', 1);
-			$this->Cell(35, 5, 'Nombre', 1);
-			$this->Cell(30, 5, 'Telefono', 1);
-			$this->Cell(55, 5, 'Direccion', 1);
-			$this->Cell(35, 5, 'CC/CE/PASAPORTE', 1);
-			$this->Cell(10, 5, 'Edad', 1);
-			$this->Ln();
-		}
-
-		// Pie de página
-		function Footer(){
-		    // Posición: a 1,5 cm del final
-		    $this->SetY(-15);
-		    // Arial italic 8
-		    //$this->SetFont('Arial','I',8);
-		    // Número de página
-		    $this->Cell(0,10,'Page '.$this->PageNo().'/{nb}',0,0,'C');
-		}
-	}
-
-	if ($resQuery) {
-			$pdf = new PDF();
-			$pdf->AliasNbPages();
-			$pdf->AddPage();
-			$pdf->SetFont('Arial', '', 6);
-			$pdf->SetY(15);
-			foreach($resQuery as $d){
-				$customFields = json_decode($d->customFields);
-				$info = json_decode($d->info);
-				$pdf->Cell(30, 5, $fullTime, 1);
-				$pdf->Cell(35, 5, "{$info->firstName} {$info->lastName}", 1);
-				$pdf->Cell(30, 5, $info->phone, 1);
-
-				foreach ($customFields as $c) {
-					if ($c->label == "Dirección") {
-						$pdf->Cell(55, 5, $c->value, 1);
-					}
-
-					if ($c->label == "CC/CE/PASAPORTE") {
-						$pdf->Cell(35, 5, $c->value, 1);
-					}
-
-					if ($c->label == "Edad") {
-						$pdf->Cell(10, 5, $c->value, 1);
-					}
-				}
-				
-				$pdf->Ln();
-			}
-			
-			$pdf->Output("reservas_{$fullTime}.pdf", 'd');
-	}else{
-		echo "No hay reservaciones para la fecha indicada";
-	}
-
-}
-
-function medicalStatement($d, $t){
-	global $wpdb;
-	$hora = date('h:i A', strtotime($t));
-	$fecha = $d;
-
-	$date = date("Y-m-d", strtotime($d));
-	$time = date('H', strtotime($t));
-	$time = $time + 5;
-	
-	$appointment = $wpdb->prefix . 'amelia_appointments';
-	$booking = $wpdb->prefix . 'amelia_customer_bookings';
-
-	$query = " SELECT `bookingStart`,`bookingEnd`, `customFields`, `info` FROM $appointment INNER JOIN $booking ON $appointment.`ID` = $booking.`appointmentId` WHERE `bookingStart` LIKE '%$date $time%' ";
-
-	$resQuery =  $wpdb->get_results($query);
-
-	class PDF extends FPDF{
-		// Cabecera de página
-		function Header(){
-			$this->SetFont('Times', '', 8);
-			$this->Ln(5);
-			$this->Cell(50, 4, '1-. ¿Tienes malestar o dolor de garganta?', 0);
-			$this->Cell(65, 4, '2-. ¿Tienes sensacion de fatiga o cansancio muscular?', 0);
-			$this->Cell(35, 4, '3-. ¿Tienes fiebre? (+38°C)', 0);
-			$this->Cell(45, 4, '4-. ¿Tienes tos seca y persistente?', 0);
-			$this->Cell(35, 4, '5-. ¿Dificultad para respirar?', 0);
-			$this->Ln();
-			$this->Cell(55, 4, '6-. ¿Tienes secreciones o congestión nasales?', 0);
-			$this->Cell(50, 4, '7-. ¿Tienes pérdida del olfato y/o el gusto?', 0);
-			$this->Cell(85, 4, '8-. ¿Vives con alguien sospechoso o confirmado de tener COVID-19?', 0);
-			$this->Ln();
-			$this->Cell(120, 4, '9-. ¿En los últimos 14 días has tenido contacto estrecho con alguien sospechoso o confirmado de tener COVID-19?', 0);
-			$this->Ln();
-			$this->SetFont('Arial', 'B', 8);
-			$this->Ln();
-			$this->Cell(35, 4, 'Nombre', 1);
-			$this->Cell(28, 4, 'Fecha Eucaristía', 1);
-			$this->Cell(28, 4, 'Hora Eucaristía', 1);
-			$this->Cell(25, 4, 'Telefono', 1);
-			$this->Cell(15, 4, 'Campo 1', 1);
-			$this->Cell(15, 4, 'Campo 2', 1);
-			$this->Cell(15, 4, 'Campo 3', 1);
-			$this->Cell(15, 4, 'Campo 4', 1);
-			$this->Cell(15, 4, 'Campo 5', 1);
-			$this->Cell(15, 4, 'Campo 6', 1);
-			$this->Cell(15, 4, 'Campo 7', 1);
-			$this->Cell(15, 4, 'Campo 8', 1);
-			$this->Cell(15, 4, 'Campo 9', 1);
-			$this->Cell(30, 4, 'Firma', 1);
-			$this->Ln();
-		}
-
-		// Pie de página
-		function Footer(){
-		    // Posición: a 1,5 cm del final
-		    $this->SetY(-15);
-		    // Arial italic 8
-		    $this->SetFont('Arial','I',8);
-		    // Número de página
-		    $this->Cell(0,10,'Page '.$this->PageNo().'/{nb}',0,0,'C');
-		}
-	}
 
 
-	if ($resQuery) {
-			$pdf = new PDF('L');
-			$pdf->AliasNbPages();
-			$pdf->AddPage();
-			$pdf->SetY(35);
-			$pdf->SetFont('Times', '', 8);
-			foreach($resQuery as $d){
-				$info = json_decode($d->info);
-				$pdf->Cell(35, 5, "{$info->firstName} {$info->lastName}", 1);
-				$pdf->Cell(28, 5, $fecha, 1);
-				$pdf->Cell(28, 5, $hora, 1);
-				$pdf->Cell(25, 5, $info->phone, 1);	
-				$pdf->Cell(15, 5, '', 1);		
-				$pdf->Cell(15, 5, '', 1);		
-				$pdf->Cell(15, 5, '', 1);		
-				$pdf->Cell(15, 5, '', 1);		
-				$pdf->Cell(15, 5, '', 1);		
-				$pdf->Cell(15, 5, '', 1);		
-				$pdf->Cell(15, 5, '', 1);		
-				$pdf->Cell(15, 5, '', 1);		
-				$pdf->Cell(15, 5, '', 1);		
-				$pdf->Cell(30, 5, '', 1);			
-				$pdf->Ln();
-			}
-
-			$pdf->Output("declaracion_medica{$fecha}_{$hora}.pdf", 'd');
-	}else{
-		echo "No hay reservaciones para la fecha indicada";
-	}
-}
-
-function get_date() {
-
-	$date =	$_POST['fecha'];
-	$time = $_POST['hora'];
-
-	echo "<form method='post'>
+	$fecha =	$_POST['fecha'];
+	$hora = $_POST['hora'];
+	$IdToUpdate = $_POST['id'];
+	$NewTemp = $_POST['temp'];
+?>
+	<link rel="stylesheet" href="/wp-content/plugins/export-reservation/assets/style.css">
+	<form method='post' action='?page=export'>
 
 		<h2>Exportar Reservaciones</h2>
 		
@@ -226,23 +55,134 @@ function get_date() {
 		<input type='date' name='fecha' id='date' required>
 
 		<label for='time'> Hora</label>
-		<input type='time' name='hora' id='time' required>";
+		<input type='time' name='hora' id='time' required>
+
+		<?php echo submit_button('Enviar');?>
+	</form>
+
+<?php
+	$blogName = get_bloginfo('name');
+	
+	if ($fecha && $hora) {
+	?>
+		 <hr> 
+		 <p class='submit'> 
+		 	<a class='button button-success' target='_blank' href='/wp-content/plugins/export-reservation/booking-export.php<?php echo "?date=$fecha&time=$hora";?>'>
+		 		Exportar reservaciones
+		 	</a>
+		 </p>
+
+	<?php
+		$date = date("Y-m-d", strtotime($_POST['fecha']));
+		$time = date('H', strtotime($_POST['hora']));
+		$time = $time + 5;
+
+		$appointment = $wpdb->prefix . 'amelia_appointments';
+		$booking = $wpdb->prefix . 'amelia_customer_bookings';
+		$user = $wpdb->prefix . 'amelia_users';
+
+		$query = " SELECT $booking.`id`, `bookingStart`,`bookingEnd`, `customFields`, `info`, `email` FROM $appointment, $booking, $user WHERE $appointment.`ID` = $booking.`appointmentId` AND $booking.`customerId` = $user.`id` AND `bookingStart` LIKE '%$date $time%' ORDER BY $booking.`customerId` ASC  ";
+
+		$resQuery =  $wpdb->get_results($query);
+	?>
 
 	
-	$btn = submit_button('Enviar');
-	echo "{$btn}</form>";
 
-	
-	if ($_POST['fecha'] && $_POST['hora']) {
-		echo "<hr> <p class='submit'> <a class='button button-primary' target='_blank' href='?page=export&date={$date}&time={$time}&book=true'>Exportar reservaciones</a> <a class='button button-primary' target='_blank' href='?page=export&date={$date}&time={$time}&medi=true'>Declaracion Medica</a> 
-			<a class='button button-primary' href='?page=export'>Limpiar</a></p>";
-	}
-	
-	if (isset($_GET['book'])) {
-	   	exportBooking($_GET['date'], $_GET['time']);
-	}
+		<ul>
+			<?php foreach ($resQuery as $b) {
+				$customFields = json_decode($b->customFields);
+				$info = json_decode($b->info);
+			?>
+				<li>
+					<b>Nombre:</b> <?php echo "{$info->firstName} {$info->lastName}";?>
+				</li>
+				<li>
+					<b>Telefono:</b> <?php echo  $info->phone;?>
+				</li>
+				<li>
+					<b>Email:</b> <?php echo  $b->email;?>
+				</li>
+				<?php foreach ($customFields as $c) { 
 
-	if (isset($_GET['medi'])) {
-		medicalStatement($_GET['date'], $_GET['time']);
+					if ($c->label == "Direccion") {
+						echo "<li><b>Dirección:</b> $c->value</li>";
+					}
+
+					if ($c->label == "CC/CE/PASAPORTE") {
+						echo "<li><b>CC/CE/PASAPORTE:</b> $c->value</li>";
+					}
+
+					if ($c->label == "Edad") {
+						echo "<li><b>Edad:</b> $c->value</li>";
+					}
+
+					if ($c->label == "Temperatura") {
+						$temperatura = $c->value;
+					?>
+
+						<li>
+							<b>Temperatura:</b> 
+							<?php
+								if($IdToUpdate ==  $b->id){  
+									echo $NewTemp; 
+								}else{ 
+									echo $c->value;
+								}
+							?>
+						</li>
+					<?php
+					}
+					
+				} ?>
+			<p class='submit'> 
+				<a class="thickbox button button-primary btn" href="#open-modal-<?php echo $b->id;?>">
+					Actualizar Temperatura
+				</a>
+			</p>
+
+			<div id="open-modal-<?php echo $b->id;?>" class="modal-window">
+				<div>
+					<a href="#" title="Close" class="modal-close">Cerrar</a>
+					<h1>Agregar temperatura</h1>
+				    <form action="?page=export" method="post">
+				    	<label for='temp'> Temperatura </label>
+						<input type='number' step="0.1" name='temp' id='temp'>
+						<!--<input type="hidden" value="<?php echo $temperatura; ?>" name="curerentTemp">-->
+						<input type="hidden" value="<?php echo $b->id; ?>" name="id">
+						<input type="hidden" name="fecha" value="<?php echo $fecha; ?>">
+						<input type="hidden" name="hora" value="<?php echo $hora;?>">
+						<!--<input type="hidden" value="<?php echo json_encode($customFields); ?>" name="booking">-->
+						<?php echo submit_button('Actualizar');?>
+				    </form>
+				</div>
+		    </div>
+
+			<?php 
+				if ($IdToUpdate ==  $b->id) {
+					updateBooking($b->id, $temperatura, $NewTemp, json_encode($customFields));
+				}
+			?>
+
+			 <hr>
+			<?php } ?>
+		</ul>
+
+<?php	
 	}
 }
+
+function updateBooking($id, $currentTemp, $temp, $booking){
+	global $wpdb;
+	$table = $wpdb->prefix . 'amelia_customer_bookings';
+
+	$update = str_replace('"Temperatura","value":"'. $currentTemp .'"', '"Temperatura","value":"' . $temp . '"', $booking);
+	
+	$queryUpdate = "UPDATE $table SET `customFields` = '$update' WHERE $table.`id` = $id";
+
+	$newResult = $wpdb->get_results($queryUpdate);
+
+	return $newResult;
+}
+
+/*$boo = updateBooking(20, 50, $resQuery[0]->customFields);
+			print_r($boo);*/
